42af312a5e82e4dc511f05d452dcf3f39a49fe5d
jmj <waysoleward01@naver.com>
2025-06-11 03:25:44 +0900

    feat: assign1

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,14 +1,40 @@
+"use client";
+
+import Header from "@/component/layout/Header";
+import { useUser } from "@/context/UserContext";
+import Link from "next/link";
+
 // 과제 1: 마이페이지 구현
 export default function MyPage() {
   // 1.1. UserContext를 활용한 Mypage 구현 (UserContext에 아이디(userId: string), 나이(age: number), 핸드폰번호(phoneNumber: string) 추가)
+  const { user } = useUser();
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-50">
       {/* 1.2. Header Component를 재활용하여 Mypage Header 표기 (title: 마이페이지) */}
-      <p>마이페이지</p>
+      <Header title="마이페이지" />
       {/* Mypage 정보를 UserContext 활용하여 표시 (이름, 아이디, 나이, 핸드폰번호 모두 포함) */}
-
+      <div className="bg-white shadow-md p-6 mt-6 rounded-md w-80 text-left space-y-2">
+        <p>
+          <strong>이름:</strong> {user.name}
+        </p>
+        <p>
+          <strong>아이디:</strong> {user.userId}
+        </p>
+        <p>
+          <strong>나이:</strong> {user.age}
+        </p>
+        <p>
+          <strong>핸드폰번호:</strong> {user.phoneNumber}
+        </p>
+      </div>
       {/* 1.3. 홈으로 가기 버튼 구현(Link or Router 활용) */}
+      <Link
+        href="/"
+        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+      >
+        홈으로 가기
+      </Link>
     </div>
   );
 }
--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -5,10 +5,12 @@ import { createContext, ReactNode, useContext, useState } from "react";
 
 // User
 interface User {
+  userId: string;
   name: string;
-  // age: number
-  // 추가하고 싶은 속성들 ...
+  age: number;
+  phoneNumber: string;
 }
+
 // UserContextType
 interface UserContextType {
   user: User;
@@ -22,7 +24,13 @@ export const UserContext = createContext<UserContextType | undefined>(
 
 // 2. Provider 생성
 export const UserProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User>({ name: "" });
+  const [user, setUser] = useState<User>({
+    userId: "202102706",
+    name: "jeongjoonmo",
+    age: 1,
+    phoneNumber: "010-1234-1234",
+  });
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
@@ -31,11 +39,9 @@ export const UserProvider = ({ children }: { children: ReactNode }) => {
 };
 
 // 3. user 정보를 사용하기 위한 custom hook
+
 export const useUser = () => {
   const context = useContext(UserContext);
-  // 에러처리
-  if (!context) {
-    throw new Error("error");
-  }
+  if (!context) throw new Error("No UserContext");
   return context;
 };
